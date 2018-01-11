881d1968daeeed444dd4b18b8fb9fb93a70dd433
Bin Zhou <zhoubinjolley@gmail.com>
2018-01-11 17:16:21 +0800

    Add files via upload

---
+++ QY.Admin/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using QY.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace QY.Admin
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<ApplicationDbContext>(new QY.Admin.Web.Models.IdentityConfig.ApplicationDbInitializer());
+            CreateAutoMappings();
+        }
+
+        private void CreateAutoMappings()
+        {
+            new Logic.DAI.MapperCreation().CreateMaps();
+        }
+    }
+}
---
+++ QY.Admin/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using QY.Admin.Models;
+
+[assembly: OwinStartupAttribute(typeof(QY.Admin.Startup))]
+namespace QY.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
